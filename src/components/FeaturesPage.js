94b3d5c868a48b817c71144084c0f27e36c48c5f
Jacob Cavazos <jjcav84@gmail.com>
2018-12-25 18:36:25 -0600

    inting and adding in MDBContainer and adjusting margin widths in the x direction for certain pages where the class attribute was missing

--- src/components/FeaturesPage.js
+++ src/components/FeaturesPage.js
@@ -1,32 +1,33 @@
-import React from "react";
-import {  MDBRow, MDBCol, MDBIcon, Animation } from "mdbreact";
+import React from 'react';
+import {  MDBRow, MDBCol, MDBIcon, Animation, MDBContainer } from 'mdbreact';
 
 const FeaturesPage = () => {
   return (
-      <section id="about" className="text-center my-5">
-      <MDBRow>
-        <MDBCol size="1"></MDBCol>
-        <MDBCol size="10">
-          <Animation reveal type="zoomIn" duration="1s">
-            <MDBIcon className="cyan-text mb-2" icon="user" size="4x" />
-          </Animation> 
-          <h3 className="responsive font-weight-bold my-5">
+    <section id="about" className="text-center my-5">
+      <MDBContainer>
+        <MDBRow>
+          <MDBCol size="1"></MDBCol>
+          <MDBCol size="10">
+            <Animation reveal type="zoomIn" duration="1s">
+              <MDBIcon className="cyan-text mb-2" icon="user" size="4x" />
+            </Animation> 
+            <h3 className="responsive font-weight-bold my-5">
             About Me
-          </h3>
-          <p className="lead black-text mx-auto mb-5">
+            </h3>
+            <p className="lead black-text mx-auto mb-5">
             Hello, my name is Jacob Cavazos and thank you visiting my personal website. I am a high-performing results-driven IT Operations and Software Development Engineer with comprehensive experience across multiple IT projects, web application development and integration, infrastructure management, and quality assurance in dynamic, high-impact, and fast-paced environments. I thrive in driving business growth, directing creative and technical initiatives, providing operational support, and implementing cutting-edge information solutions to address business opportunities and organizational growth.
-          </p>
+            </p>
           </MDBCol>
           <MDBCol size="1">
           </MDBCol>
-          </MDBRow>
-          <MDBRow className="ml-5 mr-5 pl-2 pr-2">
-            <MDBCol lg="4" className="mx-auto">
+        </MDBRow>
+        <MDBRow className="ml-5 mr-5 pl-2 pr-2">
+          <MDBCol lg="4" className="mx-auto">
             <Animation reveal type="zoomIn" duration="1s">
               <MDBIcon className="cyan-text" icon="plane" size="3x" />
-              </Animation>  
-              <h5 className="font-weight-bold my-4">Adventure</h5>
-              <p className="black-text mb-lg-0 mb-5">
+            </Animation>  
+            <h5 className="font-weight-bold my-4">Adventure</h5>
+            <p className="black-text mb-lg-0 mb-5">
                 I believe in enjoying life and the importance of being in the the moment so I aim to live with a sense of adventure. I am not afraid of failure so by extending myself beyond my level of comfort in a controlled manner. I am always pushing to learn continuously through direct experience. I enjoy learning as much as I can and living as an gift and treasure to be enjoyed because to me my memories are more precious than luxurious material possessions which seem to come and go with the ebb and flow of time. Each day truly brings about new opportunities for education and adventures which should be seized upon in fulfillment of a well lived and meaningful existence.
               </p>
             </MDBCol>
@@ -40,17 +41,18 @@ const FeaturesPage = () => {
             </p>
           </MDBCol>
           <MDBCol lg="4" className="mx-auto">
-          <Animation reveal type="zoomIn" duration="1s">
-            <MDBIcon className="cyan-text" icon="smile-o" size="3x" />
-          </Animation>  
+            <Animation reveal type="zoomIn" duration="1s">
+              <MDBIcon className="cyan-text" icon="smile-o" size="3x" />
+            </Animation>  
             <h5 className="font-weight-bold my-4">Happiness</h5>
             <p className="black-text mb-lg-0 mb-5">
               Philosophy is very important to me. Throughout my life I have struggled for purpose and sought for meaning as we all must  and within that struggle I have found that the only true meaning I have garnered and which brings me a happy and purposeful existence is a dedication to responsibility and stewardship. I firmly believe in assisting others, sharing, teaching, and generally leaving things in a better state than when I first encounter them. I feel that by our very existence we are burdened with this task and that it is instrumental to self fulfillment. Experience has taught me that you always get back more than you give away and so I am driven by this underpinning to work hard in dedicated to life long learning.
             </p>
           </MDBCol>
         </MDBRow>
-      </section>
-    );
-  }
+      </MDBContainer>
+    </section>
+  );
+};
     
-    export default FeaturesPage;  
+export default FeaturesPage;  
