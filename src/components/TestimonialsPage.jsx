d8878db82aee0a6587f1236251b6d4ec04e31eb6
Jacob Cavazos <jjcav84@gmail.com>
2018-12-25 19:26:57 -0600

    more grid adjusting

--- src/components/TestimonialsPage.jsx
+++ src/components/TestimonialsPage.jsx
@@ -11,7 +11,7 @@ const TestimonialsPage = () => {
 
         <MDBCarousel
           activeItem={1}
-          length={2}
+          length={1}
           testimonial={true}
           interval={false}
           showIndicators={true}
@@ -34,23 +34,6 @@ const TestimonialsPage = () => {
                 <MDBIcon icon="star" className="light-blue-text" />
                 <MDBIcon icon="star-half-full" className="light-blue-text" />
               </MDBTestimonial> 
-            </MDBCarouselItem>
-            <MDBCarouselItem itemId="2">
-              <MDBTestimonial>
-                <p className="indigo-text">
-                  <MDBIcon icon="quote-left" /> I have known Jacob Cavazos for about eight years. In that time he impressed me with his technical skills and knowledge. His willingness to embrace new technologies and his stark enthusiasm for technology are almost unbelievable as they are unwavering. Jacob played a lead role in the success of our projects and the subsequent support of solutions. It was through Jacob´s skills and knowledge as well as his ability to lead technical discussions, to assist and direct others with his energy and drive to deliver against difficult goals at the center of Alzheimer's and Parkinson's disease research by leveraging his technical sophistication to accelerate our tempo as a behind the scenes facilitator. Jacob was the key technical resource in my group. Jacob taught and mentored M.S. and Ph.D. students and helped also a postdoctoral scientist in his technological tasks. Jacob was one of the finest people I have worked with. He is a very hard working, focused and motivated individual with expertise in many various areas of technology especially Linux operations.
-                  <MDBIcon icon="quote-right" />
-                </p>
-                <h4 className="font-weight-bold my-1 pt-2 indigo-text">Professor Dr. Orkid Coskuner Weber</h4>
-                <h6 className="font-weight-bold my-3 indigo-text">
-                Turkish-German University Molecular Biotechnology
-                </h6>
-                <MDBIcon icon="star" className="indigo-text" />
-                <MDBIcon icon="star" className="indigo-text" />
-                <MDBIcon icon="star" className="indigo-text" />
-                <MDBIcon icon="star" className="indigo-text" />
-                <MDBIcon icon="star-half-full" className="indigo-text" />
-              </MDBTestimonial>
             </MDBCarouselItem>
           </MDBCarouselInner>
         </MDBCarousel>
