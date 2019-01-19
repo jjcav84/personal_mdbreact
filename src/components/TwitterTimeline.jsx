002b414e3ec2ea036c1996a7e8cde73d10af49bb
Jacob Cavazos <jjcav84@gmail.com>
2019-01-19 04:39:51 -0600

    twitterTimeline linting

--- src/components/TwitterTimeline.jsx
+++ src/components/TwitterTimeline.jsx
@@ -5,17 +5,17 @@ import { MDBContainer, MDBRow } from 'mdbreact';
 const TwitterTimeline = () => {
   return (
     <section id="twitter" className="text-center mx-auto my-5">
-    <MDBContainer>
-    <MDBRow className="mx-auto" center>
-    <div>
-        <TwitterTimelineEmbed sourceType="URL"          url="https://twitter.com/jcavazos84" 
-        options={{width: 500, height: 350}} />
-    </div>
-    </MDBRow>
-    </MDBContainer>
+      <MDBContainer>
+        <MDBRow className="mx-auto" center>
+          <div>
+            <TwitterTimelineEmbed sourceType="URL"          url="https://twitter.com/jcavazos84" 
+              options={{width: 500, height: 350}} />
+          </div>
+        </MDBRow>
+      </MDBContainer>
     </section>
-    );
-  }
+  );
+};
   
-  export default TwitterTimeline;  
+export default TwitterTimeline;  
 
