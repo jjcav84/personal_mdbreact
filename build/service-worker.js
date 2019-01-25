217ca2af9a357c90ce2b7f8a50116a771f712c6a
Jacob Cavazos <jjcav84@gmail.com>
2019-01-25 06:57:24 -0600

    yarn build

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.90d33054313bd9b66193049eebda15ee.js"
+  "/precache-manifest.4018299f7235cd8270190a01442d3b1f.js"
 );
 
 workbox.clientsClaim();
